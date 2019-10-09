cac07fb3808b1b794bae9a026ddb7c96076d9f52
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-10-09 16:26:57 -0400

    Creates store, connnects header component

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+// action suite - 3 things
+// 1. action creators - function that creates actions (returns action objects)
+// 2. action types - variables that contain the action type strings to save us from spelling errors
+// 3. actions - objects that describe to the reducer what event has occured and how it should update state
+
+
+export const ADD_FEATURE  = 'ADD_FEATURE'
---
+++ src/reducers/featureReducer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export const featureReducer = () => {
+    return {
+        title: 'hello world from redux store'
+    }
+}
